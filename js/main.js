0edbdbbcaa5698d81126afcb21111a296413bdcf
Golubkov Nikita <nigolubkov@gmail.com>
2020-06-11 01:24:10 +0300

    add arrey generation function, and fix getRandomIndex parameter

--- js/main.js
+++ js/main.js
@@ -20,13 +20,12 @@ var PHOTOS = [
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
 ];
-var MAX_HEIGHT = 630;
-var MIN_HEIGHT = 130;
+var MAX_Y = 630;
+var MIN_Y = 130;
 var PRICE_MIN = 10000;
 var PRICE_MAX = 50000;
-var PIN_WIDTH = 50 / 2;
-var countNumber = 8;
-var similarPinTemplate = document.querySelector('#pin')
+// var countNumber = 8;
+var PinTemplate = document.querySelector('#pin')
   .content
   .querySelector('.map__pin');
 var mapPins = document.querySelector('.map__pins');
@@ -45,8 +44,8 @@ var getRandomIndex = function (arrLength) {
 
 // функция для создания объекта
 var generateData = function (i) {
-  var locY = getRandomNumb(MIN_HEIGHT, MAX_HEIGHT);
-  var locX = getRandomNumb(0 + PIN_WIDTH, mapWidth - PIN_WIDTH);
+  var locY = getRandomNumb(MIN_Y, MAX_Y);
+  var locX = getRandomNumb(0, mapWidth);
 
   var ads = {
     author: {
@@ -56,14 +55,14 @@ var generateData = function (i) {
       title: 'Заголовок',
       address: locX + ', ' + locY,
       price: getRandomNumb(PRICE_MIN, PRICE_MAX),
-      type: TYPE[getRandomIndex(TYPE)],
-      rooms: getRandomNumb(1, 3),
+      type: TYPE[getRandomIndex(TYPE.length)],
+      rooms: getRandomNumb(1, 3, 100),
       guests: getRandomNumb(1, 3),
-      checkin: CHECKING_TIME[getRandomIndex(CHECKING_TIME)],
-      checkout: CHECKING_TIME[getRandomIndex(CHECKING_TIME)],
-      features: FEATURES.slice(getRandomIndex(FEATURES)),
+      checkin: CHECKING_TIME[getRandomIndex(CHECKING_TIME.length)],
+      checkout: CHECKING_TIME[getRandomIndex(CHECKING_TIME.length)],
+      features: FEATURES.slice(getRandomIndex(FEATURES.length)),
       description: 'Описание',
-      photos: PHOTOS.slice(getRandomIndex(PHOTOS)),
+      photos: PHOTOS.slice(getRandomIndex(PHOTOS.length)),
     },
     location: {
       x: locX,
@@ -73,13 +72,22 @@ var generateData = function (i) {
 
   return ads;
 };
+// Функция создания массива
+var generateMock = function (count) {
+  var arr = [];
+  for (var i = 0; i < count; i++) {
+    arr.push(generateData(i));
+  }
+  return arr;
+};
+var arrObject = generateMock(8);
 
 // Временно убираем класс скрытия с .Map
 document.querySelector('.map').classList.remove('map--faded');
 
 // функция отрисовки метки
 var renderPin = function (data) {
-  var pinElement = similarPinTemplate.cloneNode(true);
+  var pinElement = PinTemplate.cloneNode(true);
 
   pinElement.querySelector('img').src = data.author.avatar;
   pinElement.querySelector('img').alt = data.offer.title;
@@ -88,7 +96,11 @@ var renderPin = function (data) {
   return pinElement;
 };
 
-for (var i = 0; i < countNumber; i++) {
-  pinFragment.appendChild(renderPin(generateData(i)));
-}
+var generateObject = function (countObj) {
+  for (var i = 0; i < countObj; i++) {
+    pinFragment.appendChild(renderPin(arrObject[i]));
+  }
+};
+generateObject(8);
+
 mapPins.appendChild(pinFragment);

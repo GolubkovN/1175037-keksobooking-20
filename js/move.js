3394d8a8584366a4476abc396cb49433d1f2821b
Golubkov Nikita <nigolubkov@gmail.com>
2020-07-09 11:50:48 +0300

    remaked moove module

--- js/move.js
+++ js/move.js
@@ -3,33 +3,33 @@
 (function () {
   var MAIN_PIN_HEIGHT = 65;
 
+  var coordslimits = {
+    left: window.util.minX - window.util.mainPin.offsetHeight / 2,
+    right: window.util.maxX - window.util.mainPin.offsetWidth / 2,
+    top: window.util.minY + window.util.mainPin.offsetHeight - MAIN_PIN_HEIGHT,
+    bottom: window.util.maxY + window.util.mainPin.offsetHeight - MAIN_PIN_HEIGHT
+  };
+
+  var checkCoords = function () {
+    if (window.util.mainPin.offsetLeft <= coordslimits.left) {
+      window.util.mainPin.style.left = coordslimits.left + 'px';
+    }
+    if (window.util.mainPin.offsetLeft >= coordslimits.right) {
+      window.util.mainPin.style.left = coordslimits.right + 'px';
+    }
+    if (window.util.mainPin.offsetTop <= coordslimits.top) {
+      window.util.mainPin.style.top = coordslimits.top + 'px';
+    }
+    if (window.util.mainPin.offsetTop >= coordslimits.bottom) {
+      window.util.mainPin.style.top = coordslimits.bottom + 'px';
+    }
+  };
+
   window.util.mainPin.addEventListener('mousedown', function (evt) {
     evt.preventDefault();
     var startCoords = {
       x: evt.clientX,
       y: evt.clientY
-    };
-
-    var coordslimits = {
-      left: window.util.minX - window.util.mainPin.offsetHeight / 2,
-      right: window.util.maxX - window.util.mainPin.offsetWidth / 2,
-      top: window.util.minY + window.util.mainPin.offsetHeight - MAIN_PIN_HEIGHT,
-      bottom: window.util.maxY + window.util.mainPin.offsetHeight - MAIN_PIN_HEIGHT
-    };
-
-    var checkCoords = function () {
-      if (window.util.mainPin.offsetLeft <= coordslimits.left) {
-        window.util.mainPin.style.left = coordslimits.left + 'px';
-      }
-      if (window.util.mainPin.offsetLeft >= coordslimits.right) {
-        window.util.mainPin.style.left = coordslimits.right + 'px';
-      }
-      if (window.util.mainPin.offsetTop <= coordslimits.top) {
-        window.util.mainPin.style.top = coordslimits.top + 'px';
-      }
-      if (window.util.mainPin.offsetTop >= coordslimits.bottom) {
-        window.util.mainPin.style.top = coordslimits.bottom + 'px';
-      }
     };
 
     var onMouseMove = function (moveEvt) {
